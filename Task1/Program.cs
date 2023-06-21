70ea5dbd71986ba4435c09671d0b5b8c4d4b12eb
ShaMaxDev <maksimbychkov@yahoo.com>
2023-06-20 20:40:40 -0700

    Final

---
+++ Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Task1
+{
+    internal class Character
+    {
+        static void Main(string[] args)
+        {
+            string nameCharacter = "Legolas";
+            string classCharacter = "Archer";
+            char gradeCharacter = 'A';
+            ushort ageCharacter = 825;
+            byte powerCharacter = 25;
+            float criticalHitChanseCharacter = 0.75f;
+            uint moneyCharacter = 3825;
+            sbyte karmaCharacter = -5;
+            bool goodCharacter = karmaCharacter > 0;
+            short healthPointCaracter = 10000;
+            bool aliveCharacter = healthPointCaracter > 0;
+            int arrowQuantityCarecter = 2140000000;
+            bool brokenUniverse = arrowQuantityCarecter < 0;
+
+
+        }
+    }
+}
